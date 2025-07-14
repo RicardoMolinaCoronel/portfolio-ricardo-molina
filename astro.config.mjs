b7bf604e5f4d79c9758e3245b2296fd80795d34e
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2025-07-13 19:10:30 -0500

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+//import tailwind from '@astrojs/tailwind'
+
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -8,5 +10,8 @@ export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://ricardomolinacoronel.tech', // tu dominio personalizado
+  base: '/',
+  outDir: 'dist',
 });
